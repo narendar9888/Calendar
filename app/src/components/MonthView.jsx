dd0b267b3b3acc56de368d97fa1bd6b08e39b0c1
Narendar Kumar <229602725+narendar9888@users.noreply.github.com>
2026-09-08 22:24:36 +0530

    complete month page

--- app/src/components/MonthView.jsx
+++ app/src/components/MonthView.jsx
@@ -12,12 +12,34 @@ import {
 
 const MonthView = () => {
     const {
-        currentDate
+        currentDate,
+        events,
+        search,
+        category,
+        selectedDate,
+        setSelectDate,
+        openModal,
     } = useCalendar();
 
 
     const calendar = getMonthGrid(currentDate);
 
+    const filteredEvents = events.filter((event) =>{
+        const matchSearch = event.title
+        .toLowerCase()
+        .includes(search.toLowerCase());
+
+        const matchCategory = 
+            category === "All" || event.category === category;
+
+        return matchCategory && matchSearch;
+    })
+
+    const getEvent = (date) => {
+        return filteredEvents.filter((event) => 
+            isSameDay(new Date(event.date), date)
+        );   
+    };
 
     return (
         <div className="month-view">
@@ -34,28 +56,74 @@ const MonthView = () => {
 
 
             <div className="month-grid">
-                {calendar.flat().map((Date, index) => {
-                    // const dayEvent = getEvent(date);
+                {calendar.flat().map((date, index) => {
+                    const dayEvents = getEvent(date);
 
                     const isCurrentMonth = 
-                        Date.getMonth() === currentDate.getMonth();
+                        date.getMonth() === currentDate.getMonth();
 
                     return (
                         <div
                             key={index}
-
+                            className={`day-cell
+                                ${!isCurrentMonth ? "other-month" : ""}
+                                ${
+                                    isToday(date)
+                                        ? "today"
+                                        : ""
+                                }
+                                ${
+                                    isSameDay(
+                                        date, 
+                                        selectedDate
+                                    )
+                                        ? "selected"
+                                        : ""
+                                }`}
+                            onClick={() => 
+                                setSelectDate(date)
+                            }
                         >
                             <div className="day-number">
-                                {Date.getDate()}
+                                {date.getDate()}
+                            </div>
+
+                            <div className="events">
+                                {dayEvents
+                                    .slice(0, 3)
+                                    .map((event) => (
+                                        <div
+                                            key={event.id}
+                                            className="event"
+                                            style={{
+                                                background:
+                                                    event.color  
+                                            }}
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                openModal(event);
+                                            }}
+                                        >
+                                            {event.time && 
+                                            `${event.title}`}
+                                            {event.title}
+                                        </div>
+                                    ))}
+
+                                {dayEvents.length > 3 && (
+                                    <div className="more-event">
+                                        +{dayEvents.length - 3} more
+                                    </div>
+                                )}
                             </div>
                         </div>
-                    )
+                    );
                 })}
             </div>
 
 
         </div>
     )
-}
+};
 
 export default MonthView;
